ee2ed068e9064680701606ca379e14ad2bdff546
caiqichang <cai_qichang@qq.com>
2024-11-11 22:27:46 +0800

    add logo icon

---
+++ src/main/kotlin/caiqichang/mybatisuite/Icon.kt
@@ -0,0 +1,7 @@
+package caiqichang.mybatisuite
+
+import com.intellij.openapi.util.IconLoader
+
+object Icon {
+    val LOGO = IconLoader.getIcon("/META-INF/pluginIcon_13x13.png", Icon::class.java)
+}
