8ff05b48812e429ea291b5129cf787d98d0c9456
caiqichang <cai_qichang@qq.com>
2024-11-12 17:32:23 +0800

    restructure code navigation

--- src/main/kotlin/caiqichang/mybatisuite/MapperFileDescription.kt
+++ src/main/kotlin/caiqichang/mybatisuite/MapperFileDescription.kt
@@ -1,5 +1,14 @@
 package caiqichang.mybatisuite
 
+import com.intellij.openapi.module.Module
+import com.intellij.psi.xml.XmlFile
 import com.intellij.util.xml.DomFileDescription
 
-class MapperFileDescription : DomFileDescription<Mapper>(Mapper::class.java, "mapper")
+class MapperFileDescription : DomFileDescription<Mapper>(Mapper::class.java, "mapper") {
+
+    val enable = true
+
+    override fun isMyFile(file: XmlFile, module: Module?): Boolean {
+        return enable && super.isMyFile(file, module)
+    }
+}
